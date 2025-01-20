069b980ff9e383ff693cd9aafd4307c4e33f6bbb
Bianor Araujo <bianor.araujo@gmail.com>
2025-01-20 15:07:40 +0000

    Add reset password endpoint

--- src/Emerald.Api/ViewModels/UserViewModel.cs
+++ src/Emerald.Api/ViewModels/UserViewModel.cs
@@ -29,6 +29,21 @@ namespace Emerald.Api.ViewModels
         public string Password { get; set; } = string.Empty;
     }
 
+    public class ResetPasswordViewModel
+    {
+        [Required(ErrorMessage = "The field {0} is required")]
+        [EmailAddress(ErrorMessage = "The field {0} is in an invalid format")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(100, ErrorMessage = "The field {0} must be between {2} and {1} characters", MinimumLength = 6)]
+        public string CurrentPassword { get; set; } = string.Empty;
+        
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(100, ErrorMessage = "The field {0} must be between {2} and {1} characters", MinimumLength = 6)]
+        public string NewPassword { get; set; } = string.Empty;
+    }
+
     public class LoginResponseViewModel
     {
         public string AccessToken { get; set; } = string.Empty;
